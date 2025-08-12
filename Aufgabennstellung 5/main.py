8bad5ecede8f752713058cecb83eb17fdc8005a4
SchreibSchreib <felixschreiber89@gmail.com>
2025-08-12 17:21:33 +0200

    Finished code for task five

---
+++ Aufgabennstellung 5/main.py
@@ -0,0 +1,16 @@
+def sum_natural_numbers(max_value):
+    if not isinstance(max_value, int) or max_value < 1:
+        raise ValueError("Zahl muss eine natürliche Zahl >= 1 sein.")
+
+    def _recursive_sum(current_number):
+        if current_number == 1:
+            return 1
+        return current_number + _recursive_sum(current_number - 1)
+
+    return _recursive_sum(max_value)
+
+
+print(sum_natural_numbers(1))
+print(sum_natural_numbers(5))
+print(sum_natural_numbers(50))
+print(sum_natural_numbers(500))
