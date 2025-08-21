d2aded18829cf08716c0d3873775aff85a24b6e0
SchreibSchreib <felixschreiber89@gmail.com>
2025-08-21 14:16:38 +0200

    Refactoring

--- Aufgabennstellung 6/Python/count_even.py
+++ Aufgabennstellung 6/Python/count_even.py
@@ -1,12 +1,13 @@
-def count_even(numbers):
+def count_even(list_of_numbers):
     counter = 0
-    for number in numbers:
+    for number in list_of_numbers:
         if (number % 2) == 0:
             counter += 1
 
     return counter
 
 
-numberList = [1,2,4,8,9,10,14,6,3,5,7,9,13,14,15,16,17,18,200,101,211,122]
+number_list = [1,2,4,8,9,10,14,6,3,5,7,9,13,14,15,16,17,18,200,101,211,122]
 
-print(f"Anzahl gerader Zahlen: {count_even(numberList)}")
+print(f"Liste: {number_list}")
+print(f"Anzahl gerader Zahlen: {count_even(number_list)}")
